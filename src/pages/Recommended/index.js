f51d4e311b52ab512653dd387729968ac80c2ab0
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-10-03 07:29:50 -0400

    added content to recommended

--- src/pages/Recommended/index.js
+++ src/pages/Recommended/index.js
@@ -1,49 +1,54 @@
+import { Link } from "react-router-dom"
+
 export default function Recommended() {
     return (
-        <div>Recommended Resources</div>
+        <div>
+            <h3>Recommendended Resources</h3>
+            <br/>
+            <h4>BOOKS</h4>
+            <p>These books are available in nearly all public libraries and bookstores. They are also available at the book sales table at all Earthsave events.</p>
+            <br />
+            <p><b>"FOOD REVOLUTION"</b>, by Earthsave founder John Robbins, gives current, well-documented information on all the best reasons, including health, animal protection, and ecology, to adopt a plant centered diet. Food Revolution is similar in style and content to Robbins' classic best seller DIET FOR A NEW AMERICA.</p>
+            <br />
+            <p><b>"THE CHINA STUDY"</b>, by T. Colin Campbell, PhD, details some of the most compelling scientific evidence available on the connection between diet and a whole host of chronic diseases that are currently shortening Americans’ lives. Dr. Campbell is the nutritional biochemist from Cornell University who led the Cornell/Oxford/China Health Project, which is the most comprehensive large study ever undertaken of the relationship between diet and disease.</p>
+            <br />
+            <p><b>"THE NEW BECOMING VEGETARIAN"</b>, by Vesanto Melina, RD; Brenda Davis, RD, is a comprehensive and well-researched manual for achieving optimal nutrition at all stages of life. It includes more than 50 great tasting, easy recipes.</p>
+            <br />
+            <p><b>"THE WORLD PEACE DIET"</b>, by musician/composer, educator, and Zen Buddhist Master Will Tuttle, PhD, examines, comprehensively, the link between the violence on our dinner plates and a whole, massive cycle of violence in the world today. Tuttle sees animal agriculture as the central link to a broad range of problems - psychological, social, and spiritual, as well as health and environmental. The book offers powerful ways we can all experience healing and peace and contribute to a positive transformation of human consciousness.</p>
+            <br />
+            <p><b>OUR FOOD OUR FUTURE</b> is an excellent guide to the diverse reasons for shifting to a whole foods, plant based diet. You may view the whole booklet or download a copy for free by going to . You can also purchase a copy for just one dollar at the book table at Earthsave Baltimore potluck events.</p>
+            <br />
+            <p><b>FLIERS & HANDOUTS</b></p>
+            <br />
+            <p>You may download and print the items listed as "attachments" at the bottom of this page.</p>
+            <br />
+            <p>The first item is a flier with information on the next several Earthsave events. Page should be printed on front and reverse and folded to make small brochure. There's helpful information on popular local veg restaurants and nutrition professionals, as well as some good general information about the benefits of a whole food, plant-based diet.</p>
+            <br />
+            <p>The second item lists the nutritional recommendations of Michael Greger, MD, who is a much respected speaker and author. He's a general practitioner specilizing in clinical nutrition. Each year he reads all of the nutritional journals that are published in English and reveals his findings and recommendations.</p>
+            <br />
+            <p>The third item is an article by Dr. Dean Ornish about the connection between diet and the most common and deadly chronic diseases. The clinial studies of Dr. Ornish have shown conclusively that dietary change can not only prevent but can reverse even severe coronary artery disease.</p>
+            <br />
+            <p><b>LINKS to Helpful Websites on...</b></p>
+            <br />
+            <p><b>~VEGETARIANISM & VEGANISM~</b></p>
+            <br />
+            <p><b>Earthsave International</b>offers an opportunity to learn and grow with VegPledge, and also features Earthsave TV online educational videos and many helpful publications you may read for free online.</p>
+            <Link to="//earthsave.org/">http://earthsave.org/</Link>
+            <br />
+            <p><b>North American Vegetarian Society</b>hosts Vegetarian Summerfest, in Johnstown, PA, one of the best educational opportunites available.</p>
+            <Link to="//www.navs-online.org/"> http://www.navs-online.org/</Link>
+            <br />
+            <p><b>Vegetarian Resource Group</b>has a real helpful website that includes a nationwide guide to vegetarian and veg-friendly restaurants.</p>
+            <Link to="//www.vrg.org/"> http://www.vrg.org</Link>
+            <br />
+            <p><b>VegSource</b>website offers free online viewings of many helpful educational videos.</p>
+            <Link to="//www.vegsource.org">http://www.vegsource.org/</Link>
+            <br />
+            <p><b>T. Colin Campbell Foundation Newsletter:</b>Dr. Campbell is the nutritional biochemist from Cornell University who led the huge and comprehensive Cornell/Oxford/China Health Project, and who wrote the book <b>China Study</b>. That study provided some of the best scientific documentation showing the health sustaining effects of a plant centered diet. The newsletter lets us connect with Dr. Campbell and learn about healthy eating. See . You may subscribe by writing info=tcolincampbell.org@mcsv9.net.</p>
+        </div>
     )
 }
 
-
-// // BOOKS
-// // These books are available in nearly all public libraries and bookstores. They are also available at the book sales table at all Earthsave events.
- 
-
-// // "FOOD REVOLUTION", by Earthsave founder John Robbins, gives current, well-documented information on all the best reasons, including health, animal protection, and ecology, to adopt a plant centered diet. Food Revolution is similar in style and content to Robbins' classic best seller DIET FOR A NEW AMERICA.
-
-// // "THE CHINA STUDY", by T. Colin Campbell, PhD, details some of the most compelling scientific evidence available on the connection between diet and a whole host of chronic diseases that are currently shortening Americans’ lives. Dr. Campbell is the nutritional biochemist from Cornell University who led the Cornell/Oxford/China Health Project, which is the most comprehensive large study ever undertaken of the relationship between diet and disease.
-
-// // "THE NEW BECOMING VEGETARIAN", by Vesanto Melina, RD; Brenda Davis, RD, is a comprehensive and well-researched manual for achieving optimal nutrition at all stages of life. It includes more than 50 great tasting, easy recipes.
-
-// // "THE WORLD PEACE DIET", by musician/composer, educator, and Zen Buddhist Master Will Tuttle, PhD, examines, comprehensively, the link between the violence on our dinner plates and a whole, massive cycle of violence in the world today. Tuttle sees animal agriculture as the central link to a broad range of problems - psychological, social, and spiritual, as well as health and environmental. The book offers powerful ways we can all experience healing and peace and contribute to a positive transformation of human consciousness.
-
-// // OUR FOOD OUR FUTURE is an excellent guide to the diverse reasons for shifting to a whole foods, plant based diet. You may view the whole booklet or download a copy for free by going to . You can also purchase a copy for just one dollar at the book table at Earthsave Baltimore potluck events.
-
-// // FLIERS & HANDOUTS
-// // You may download and print the items listed as "attachments" at the bottom of this page.
-
-// // The first item is a flier with information on the next several Earthsave events. Page should be printed on front and reverse and folded to make small brochure. There's helpful information on popular local veg restaurants and nutrition professionals, as well as some good general information about the benefits of a whole food, plant-based diet.
-
-// // The second item lists the nutritional recommendations of Michael Greger, MD, who is a much respected speaker and author. He's a general practitioner specilizing in clinical nutrition. Each year he reads all of the nutritional journals that are published in English and reveals his findings and recommendations.
-
-// // The third item is an article by Dr. Dean Ornish about the connection between diet and the most common and deadly chronic diseases. The clinial studies of Dr. Ornish have shown conclusively that dietary change can not only prevent but can reverse even severe coronary artery disease.
- 
-
-// // LINKS to Helpful Websites on...
-
-// // ~VEGETARIANISM & VEGANISM~
-
-// // Earthsave International offers an opportunity to learn and grow with VegPledge, and also features Earthsave TV online educational videos and many helpful publications you may read for free online.
-// // http://earthsave.org/
-
-// // North American Vegetarian Society hosts Vegetarian Summerfest, in Johnstown, PA, one of the best educational opportunites available. http://www.navs-online.org/
-
-// // Vegetarian Resource Group has a real helpful website that includes a nationwide guide to vegetarian and veg-friendly restaurants.
-// // http://www.vrg.org
-
-// // VegSource website offers free online viewings of many helpful educational videos. http://www.vegsource.org/
-
-// // T. Colin Campbell Foundation Newsletter: Dr. Campbell is the nutritional biochemist from Cornell University who led the huge and comprehensive Cornell/Oxford/China Health Project, and who wrote the book China Study. That study provided some of the best scientific documentation showing the health sustaining effects of a plant centered diet. The newsletter lets us connect with Dr. Campbell and learn about healthy eating. See . You may subscribe by writing info=tcolincampbell.org@mcsv9.net.
 
  
 // // ~ANIMAL ADVOCACY~
