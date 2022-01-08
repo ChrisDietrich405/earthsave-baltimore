4100313c85116e24a71a4c0f7729aec0d59870bb
root <root@LAPTOP-GK52SLMD.localdomain>
2022-01-08 11:28:04 -0500

    improving contact style

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,95 +1,123 @@
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 
-import "./styles.css"
+import "./styles.css";
 
 export default function Contact() {
-    return (
-        <div className="page-container">
-            <h2>Contact</h2>
-            <p>EARTHSAVE BALTIMORE</p>
-            <p>Organizer: Don Robertson</p>
-            <p>Email Earthsave Baltimore at Baltimore@Earthsave.org, call us at 410-252-3043, or write us at 517 Talbott Ave., Lutherville, MD, 21093. Website is</p>
-            <br />
-            <p>EARTHSAVE INTERNATIONAL</p>
-            <p>Board President: Jeff Nelson</p>
-            <p>20555 Devonshire St., Ste. 105,</p>
-            <p>Chatsworth, CA 91311,</p>
-            <br />
-            <a href="http://www.earthsave.org/">http://www.earthsave.org/</a>
-            <br />
-            <br />
-            <p>EARTHSAVE CHAPTERS AND AFFILIATES</p>
-            <br />
-            <p>Canada</p>
-            <p>Vancouver: 2150 Maple Street, Vancouver, BC V6J 3T3 CANADA</p>
-            <p>Phone: (604) 731-5885</p>
-            <p>FAX: 604-731-5805</p>
-            <p>Email: office@earthsave.ca</p>
-            <p>Website:</p>
-            <br />
-            <p>Colorado</p>
-            <p>Dr. Connie Sanchez N.D.</p>
-            <p>Center For Health, 3190 S. Wadsworth Blvd., Ste 301, Lakewood, CO 80227</p>
-            <p>Phone: (720) 206-4440</p>
-            <p>Email: colorado@earthsave.org</p>
-            <p>Website:</p>
-            <br />
-            <p>Florida</p>
-            <p>Miami: Jeff Tucker</p>
-            <p>PO Box 160191 Miami, FL 33116-0191</p>
-            <p>Phone: (305) 228-1116</p>
-            <p>Email: miami@earthsave.org</p>
-            <p>Website:</p>
-            <br />
-            <p>Florida</p>
-            <p>Ocala: Art Konstantino</p>
-            <p>P.O. Box 670, Williston, FL 32696</p>
-            <p> Phone: (352) 528-5287</p>
-            <p>Email: ocala@earthsave.org</p>
-            <p>Website:</p>
-            <br />
-            <p>Illinois</p>
-            <p>Chicago: 2417 W Homer St, Chicago, IL 60647</p>
-            <p>Phone: (312) 924-1801</p>
-            <p>Website:</p>
-            <br />
-            <p>Kentucky</p>
-            <p>Lexington:</p>
-            <p>Phone: (859) 266-9990</p>
-            <p>Email: lexington@earthsave.org</p>
-            <br />
-            <p>Kentucky</p>
-            <p>Louisville: PO Box 4397, Louisville, KY 40202</p>
-            <p>Phone: (502) 569-1876</p>
-            <p>Email: louisville@earthsave.org</p>
-            <p>Website:</p>
-            <br />
-            <p>Minnesota</p>
-            <p>Twin Cities</p>
-            <p>Phone: (952) 930-1205</p>
-            <p>Website:</p>
-            <p>Email: twincities@earthsave.org</p>
-            <br />
-            <p> New York</p>
-            <p>New York City/Long Island: PO Box 96, New York, NY 10108</p>
-            <p>Phone: (718) 459-7503</p>
-            <p> Email: nyc@earthsave.org</p>
-            <p>Website:</p>
-            <br />
-            <p>New York</p>
-            <p>Suffolk</p>
-            <p>Email: centralsuffolk@earthsave.org</p>
-            <br />
-            <p>Ohio</p>        
-            <p>Cincinnati: John Mooter</p>        
-            <p>PO Box 3125, Cincinnati, OH 45201-3125</p>        
-            <p>Phone: (513) 929-2500</p>        
-            <p>Email: cincinnati@earthsave.org</p>        
-            <p>Website:</p>    
-            <br />
-            <p>Utah</p>    
-            <p>Salt Lake City: Jennifer Russell-Fenus</p>    
-            <p>Email: slc@earthsave.org</p>      
-        </div>
-    )   
-}   
+  return (
+    <div className="page-container">
+      <h1 className="header-title">Contact</h1>
+      <p>EARTHSAVE BALTIMORE</p>
+      <p>Organizer: Don Robertson</p>
+      <p>
+        Email Earthsave Baltimore at Baltimore@Earthsave.org, call us at
+        410-252-3043, or write us at 517 Talbott Ave., Lutherville, MD, 21093.
+        Website is
+      </p>
+      <br />
+      <p>EARTHSAVE INTERNATIONAL</p>
+      <p>Board President: Jeff Nelson</p>
+      <p>20555 Devonshire St., Ste. 105,</p>
+      <p>Chatsworth, CA 91311,</p>
+      <br />
+      <a href="http://www.earthsave.org/" target="_blank">
+        http://www.earthsave.org/
+      </a>
+      <br />
+      <br />
+      <p>EARTHSAVE CHAPTERS AND AFFILIATES</p>
+      <br />
+      <p>Canada</p>
+      <p>Vancouver: 2150 Maple Street, Vancouver, BC V6J 3T3 CANADA</p>
+      <p>Phone: (604) 731-5885</p>
+      <p>FAX: 604-731-5805</p>
+      <span>Email</span>
+      <a href="mailto: office@earthsave.ca">office@earthsave.ca</a>
+      <p>Website:</p>
+      <br />
+      <p>Colorado</p>
+      <p>Dr. Connie Sanchez N.D.</p>
+      <p>
+        Center For Health, 3190 S. Wadsworth Blvd., Ste 301, Lakewood, CO 80227
+      </p>
+      <p>Phone: (720) 206-4440</p>
+      <span>Email</span>
+      <a href="mailto: colorado@earthsave.org">colorado@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>Florida</p>
+      <p>Miami: Jeff Tucker</p>
+      <p>PO Box 160191 Miami, FL 33116-0191</p>
+      <p>Phone: (305) 228-1116</p>
+      <span>Email</span>
+      <a href="mailto: miami@earthsave.org">miami@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>Florida</p>
+      <p>Ocala: Art Konstantino</p>
+      <p>P.O. Box 670, Williston, FL 32696</p>
+      <p> Phone: (352) 528-5287</p>
+      <span>Email</span>
+      <a href="mailto: ocala@earthsave.org">ocala@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>Illinois</p>
+      <p>Chicago: 2417 W Homer St, Chicago, IL 60647</p>
+      <p>Phone: (312) 924-1801</p>
+      <p>Website:</p>
+      <br />
+      <p>Kentucky</p>
+      <p>Lexington:</p>
+      <p>Phone: (859) 266-9990</p>
+      <span>Email</span>
+      <a href="mailto: lexington@earthsave.org">lexington@earthsave.org</a>
+      <br />
+      <br />
+      <p>Kentucky</p>
+      <p>Louisville: PO Box 4397, Louisville, KY 40202</p>
+      <p>Phone: (502) 569-1876</p>
+      <span>Email</span>
+      <a href="louisville@earthsave.org">louisville@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>Minnesota</p>
+      <p>Twin Cities</p>
+      <p>Phone: (952) 930-1205</p>
+      <p>Website:</p>
+      <span>Email</span>
+      <a href="twincities@earthsave.org">twincities@earthsave.org</a>
+      <br />
+      <br />
+      <p> New York</p>
+      <p>New York City/Long Island: PO Box 96, New York, NY 10108</p>
+      <p>Phone: (718) 459-7503</p>
+      <span>Email</span>
+      <a href="nyc@earthsave.org">nyc@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>New York</p>
+      <p>Suffolk</p>
+      <span>Email</span>
+      <a href="centralsuffolk@earthsave.org">centralsuffolk@earthsave.org</a>
+      <br />
+      <br />
+      <p>Ohio</p>
+      <p>Cincinnati: John Mooter</p>
+      <p>PO Box 3125, Cincinnati, OH 45201-3125</p>
+      <p>Phone: (513) 929-2500</p>
+      <span>Email</span>
+      <a href="cincinnati@earthsave.org">cincinnati@earthsave.org</a>
+
+      <p>Website:</p>
+      <br />
+      <p>Utah</p>
+      <p>Salt Lake City: Jennifer Russell-Fenus</p>
+      <span>Email</span>
+      <a href="slc@earthsave.org">slc@earthsave.org</a>
+    </div>
+  );
+}
--- src/pages/Membership/index.js
+++ src/pages/Membership/index.js
@@ -1,24 +1,41 @@
-import Footer from "../../components/Footer"
+import Footer from "../../components/Footer";
 
 export default function Membership() {
-    return (
-        <div className="page-container">
-            <h3>Earthsave Membership</h3>
-            <br />
-            <p><b> Earthsave Members </b>take pride in supporting an organization that is influencing opinions and educating thousands about the compelling environmental, health, and humane reasons for adopting a plant based diet.</p>
-            <br />
-            <p><b>Benefits of Membership:</b></p>
-            <br />
-            <p>- Discounted fees at most Earthsave Baltimore events</p>
-            <p>- Members may display literature and make announcements at dinners</p>
-            <p>- Free half-hour session with registered dietitian Mark Rifkin</p>
-            <br />
-            <p><b>Donations</b> are <b>tax deductible,</b> but basic membership fees are not. Basic fees are <b>$35</b> for Individual, or <b>$50</b> for Family Membership, <b>$40</b> for Senior Couple, or <b>$25</b> for Student or Senior. Please make your check or money order payable to Earthsave and send to 517 Talbott Ave. Lutherville MD, 21093. Any amount beyond the basic fee is tax deductible. Thanks for your kind support!</p>
-            <br />
-            <p> For further information, call 410-252-3043 or email baltimore@earthsave.org. Hope to see you soon!</p>
-        </div>
-    )
-}
+  return (
+    <div className="page-container">
+      <h1 className="header-title">Earthsave Membership</h1>
 
+      <p>
+        <b> Earthsave Members </b>take pride in supporting an organization that
+        is influencing opinions and educating thousands about the compelling
+        environmental, health, and humane reasons for adopting a plant based
+        diet.
+      </p>
+      <br />
+      <p>
+        <b>Benefits of Membership:</b>
+      </p>
+      <br />
+      <p>- Discounted fees at most Earthsave Baltimore events</p>
+      <p>- Members may display literature and make announcements at dinners</p>
+      <p>- Free half-hour session with registered dietitian Mark Rifkin</p>
+      <br />
+      <p>
+        <b>Donations</b> are <b>tax deductible,</b> but basic membership fees
+        are not. Basic fees are <b>$35</b> for Individual, or <b>$50</b> for
+        Family Membership, <b>$40</b> for Senior Couple, or <b>$25</b> for
+        Student or Senior. Please make your check or money order payable to
+        Earthsave and send to 517 Talbott Ave. Lutherville MD, 21093. Any amount
+        beyond the basic fee is tax deductible. Thanks for your kind support!
+      </p>
+      <br />
+      <p>
+        {" "}
+        For further information, call 410-252-3043 or email
+        baltimore@earthsave.org. Hope to see you soon!
+      </p>
+    </div>
+  );
+}
 
 //
