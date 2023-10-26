2d77dc2e77e2b91308b02eee9de7b509f6ac6807
Chris <chrisdietrich3666@gmail.com>
2023-10-26 08:15:21 -0400

    added new material to news  page

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -7,31 +7,80 @@ export default function News({ className }) {
         <h1>2023 News and Events: Earth Friendly Food Choices</h1>
         <br />
         <p>
-          Great news! The Earthsave Baltimore nonprofit group will resume
-          hosting live vegan potluck meetings at Govans Presbyterian Church,
-          5828 York Road in Baltimore, near Towson. Church is next to the
-          historic Senator Theater. The potlucks will be held on the fourth
-          Saturday of each month, starting May 21st, from 6:00 until 8:15.
-          Attendees are strongly encouraged to be up-to- date with Covid shots.
+          Earthsave Baltimore is a volunteer operated nonprofit whose mission is
+          embracing a healthier, sustainable, compassionate and peaceful way of
+          eating and living. We hope you will stay in touch and join us for an
+          event soon.
+        </p>
+        <p>
+          We host vegan potluck meetings at Govans Presbyterian Church, 5828
+          York Road in Baltimore, near Towson. Church is next to the historic
+          Senator Theater. The potlucks usually meet on the fourth Saturday of
+          each month, from 6:00 until 8:15. Attendees are encouraged to be
+          up-to- date with Covid shots.
         </p>
         <br />
         <p>
-          MONTHLY VEGAN POTLUCKS: The attendance fee is $5. Please RSVP by email
-          at{" "}
-          <a href="mailto: baltimore@earthsave.org">baltimore@earthsave.org</a>,
-          or by voicemail at 410-252-3043. Guests are asked to bring a dish made
-          without animal products that will serve 8 or more. If you’d like to
-          bring food just for yourself or attend without eating, the fee is $5.
-          We can usually accommodate a few guests who wish to attend without
-          bringing a dish. The fee is $20, but pre-approval is required, and
-          we’ll need to hear from you a day or more in advance.
+          We have some fun events coming up. On Oct. <b>28th,</b> 6:00 pm, we’ll
+          present a <p>MOVIE NIGHT!</p> Vegan Potluck at Govans Presbyterian
+          Church. RSVP on Facebook at{" "}
+          <a href="https://fb.me/e/3VVHWPbFZ">Movie Night </a> and{" "}
+          <a href="https://tinyurl.com/bdfumr4e">Meetup</a>
+          MONTHLY VEGAN POTLUCKS: The attendance fee is $5. Please RSVP by
+          email.
         </p>
-        <br />
+
+        <p>
+          <b> Oct. 28th</b>, 6:00 pm: MOVIE NIGHT!{" "}
+          <b>"Harry and the Hendersons"</b>
+          (1987) is a sweet and charming family fantasy/comedy with a message of
+          kindness and compassion for animals. While returning from a trip in
+          the woods, George Henderson (John Lithgow) and his family hit a
+          bigfoot with their car. Thinking the creature is dead, they take him
+          home, but "Harry" (Kevin Peter Hall) soon awakens. Despite their
+          initial fears, Harry is a kind and sensitive being, and the Hendersons
+          become very fond of him. However, it is difficult to keep him a
+          secret, and soon they begin to fear for Harry's safety.
+        </p>
+        <p>
+          <b>Potluck attendees</b> are asked to bring a dish made without animal
+          products that will serve 8 or more. If you’d like to bring food just
+          for yourself or attend without eating, the fee is $5. We can usually
+          accommodate a few guests who wish to attend without bringing a dish.
+          The fee is $20, but pre-approval is required, and we’ll need to hear
+          from you a day or more in advance.
+        </p>
         <p>
           There’s plenty of free parking. Drive around church to large parking
-          lot, where you&#39;ll see the white Earthsave banner near the
-          Fellowship Hall entry. You can get monthly event reminders by email by
-          joining the{" "}
+          lot, where you'll see the white Earthsave banner near the Fellowship
+          Hall entry. You can get monthly event reminders by email by joining
+          the Baltimore Vegan Meetup or Baltimore Vegetarian Meetup.
+        </p>
+        <p>
+          Our next RESTAURANT GATHERING will be <b>Nov. 12th</b> at{" "}
+          <b>Taj Palace</b>
+          restaurant in Owings Mills. Their buffet, $19, is very popular with
+          our members, and vegan choices are numerous and easily located.
+          <a href="www.meetup.com/baltimore-vegan/events/296784497">
+            Restaurant Gathering
+          </a>
+        </p>
+        <p>
+          Saturday, Nov. 25th is when Earthsave’s annual THANKS-LIVING VEGAN
+          OPEN-MIC POTLUCK will happen. The last open-mic was a blast! We had
+          live music, book readings and poetry, stand up comedy and more. We
+          invite everyone, vegan or not, who would like to tell a story, sing a
+          song, tell a joke, do standup comedy, poetry, magic, or anything your
+          imagination would allow. Performers must be approved in advance.
+          Contact Don at &nbsp;
+          <a id="email-underline" href="mailto:https://www.earthsave.org">
+            Baltimore@earthsave.org
+          </a>{" "}
+          to be included in the show!
+        </p>
+        <p>
+          You can RSVP and get the latest updates for all Earthsave’s potlucks
+          and restaurant gatherings by joining one of these groups: &nbsp;
           <a
             href="https://www.meetup.com/The-Baltimore-Vegan-Meetup-Group/"
             target="_blank"
@@ -39,7 +88,7 @@ export default function News({ className }) {
           >
             Baltimore Vegan Meetup
           </a>{" "}
-          or&nbsp;
+          &nbsp;and&nbsp;
           <a
             href="https://www.meetup.com/vegetarian-9/"
             target="_blank"
@@ -47,36 +96,50 @@ export default function News({ className }) {
           >
             Baltimore Vegetarian Meetup
           </a>{" "}
-          . Another good resource for social connection, events info, questions
-          and discussion is the&nbsp;
-          <a
-            href="https://www.facebook.com/groups/EarthsaveBaltimore/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Earthsave Baltimore Facebook group
+        </p>
+        <p>
+          Please also join discussions and get event info in the Earthsave
+          <a href="www.tinyurl.com/ypwpj49h " target="_blank" rel="noreferrer">
+            &nbsp;Baltimore Facebook group.
           </a>
-          .&nbsp;
+        </p>
+        <p>
+          Saturday. Nov. 18th, 12:00 – 4:00, POPLAR SPRING ANIMAL SANCTUARY in
+          Poolesville, MD, will celebrate their annual Thanksgiving WITH the
+          Turkeys – join our friendly turkeys and all their friends in
+          celebrating a huge cruelty-free Thanksgiving potluck. It’s outdoors,
+          so you will want to dress warmly.&nbsp;
+          <a href="https://fb.me/e/2wX6BYn2c " target="_blank" rel="noreferrer">
+            POPLAR SPRING ANIMAL SANCTUARY THANKSGIVING
+          </a>
+        </p>
+        <p>
+          BALTIMORE VEGANS group on Facebook is a good place to connect with
+          like-minded people and learn about local veg/vegan friendly
+          restaurants, food sources and events.
           <a
             href="https://www.facebook.com/groups/509056979150170"
             target="_blank"
             rel="noreferrer"
           >
-            Baltimore Vegans
-          </a>{" "}
-          is another helpful resource on Facebook.
+            BALTIMORE VEGANS
+          </a>
         </p>
+        <p>
+          THE WORLD PEACE DIET: Eating for Spiritual Health and Social Harmony,
+          is a classic read, an insightful Amazon best-seller by Will Tuttle,
+          PhD. The book looks at the far-reaching implications of our food
+          choices and offers a set of universal principles for all people of
+          conscience that show how we as a species can move our consciousness
+          forward.
+        </p>
+
         <br />
         <p>
-          MONTHLY RESTAURANT MEETUPS: Earthsave continues to host Veg/Vegan
-          Meetup discussion groups on the second Saturday of each month.
-          Attendees are strongly encouraged to be up-to- date with Covid shots.
-          We will meet on alternating months at: The Taj Palace, 9419 Common
-          Brook Rd, Owings Mills, MD 21117; and Mr. Chan, 1000 Reisterstown Rd,
-          Pikesville, MD 21208. The buffet at Taj Palace is $19, and features a
-          good number of vegan and veg options. And the waitstaff is friendly
-          and helpful with answering questions. At Mr. Chan, we will order from
-          the menu.
+          There’s plenty of free parking. Drive around church to large parking
+          lot, where you'll see the white Earthsave banner near the Fellowship
+          Hall entry. You can get monthly event reminders by email by joining
+          the Baltimore Vegan Meetup or Baltimore Vegetarian Meetup.
           <br />
           <br />
           <p>Hope to see you soon!</p>
