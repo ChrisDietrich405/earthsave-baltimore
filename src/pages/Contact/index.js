f7f1670b2787a9145eacc35a582a29395c501437
Chris <chrisdietrich3666@gmail.com>
2024-04-11 10:57:51 -0400

    test

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -110,7 +110,7 @@ export default function Contact() {
       <span>Email</span>
       <a href="cincinnati@earthsave.org">cincinnati@earthsave.org</a>
 
-      <p>Website: hellogit </p>
+      <p>Website: </p>
       <br />
       <p>Utah</p>
       <p>Salt Lake City: Jennifer Russell-Fenus</p>
